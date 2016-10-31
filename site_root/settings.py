c093987c550357aaa8884429ef05b2a55dc5ea27
vadimk2016 <vadimarian@gmail.com>
2016-10-31 22:33:03 +0200

    update

--- site_root/settings.py
+++ site_root/settings.py
@@ -102,3 +102,6 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'collectstatic')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()

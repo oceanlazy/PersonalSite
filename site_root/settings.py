346053392aab19c5b734eaa8d5ccdb080a680f65
vadimk2016 <vadimarian@gmail.com>
2016-10-31 22:53:19 +0200

    update

--- site_root/settings.py
+++ site_root/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -74,14 +75,15 @@ WSGI_APPLICATION = 'site_root.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'personal_site',
         'USER': 'admin',
         'PASSWORD': ' ',
-        'HOST': 'localhost',
+        'HOST': '127.0.0.1',
         'PORT': '5432',
     }
 }
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
@@ -103,5 +105,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'collectstatic')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-import dj_database_url
-DATABASES['default'] = dj_database_url.config()
